56747e613afc0ebb7a427cf640950362b37b2cd9
hikmatyar22 <abiq5161@gmail.com>
2024-03-26 11:08:58 +0700

    Membuat prosedur luas pada fungsi main

--- PRAK4PEMDAS PT1/PRAK4PEMDAS PT1.cpp
+++ PRAK4PEMDAS PT1/PRAK4PEMDAS PT1.cpp
@@ -21,3 +21,9 @@ int main()
 	cout << "Masukkan Lebar : ";
 	cin >> lebar;
 
+	prosedurluas(panjang, lebar);
+	cout << "luas Persegi Panjang Dengan Prosedur : " << luas << endl;
+
+	cout << "Luas Persegi Panjang Dengan Fungsi : " << fungsiluas(panjang, lebar) << endl;
+
+}
